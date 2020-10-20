534439642f1dd5da66fcbc15005168e3fcffc682
jzolo22 <jzolotarev@gmail.com>
2020-10-19 21:21:37 -0400

    added notes on errors

--- app/models/cli.rb
+++ app/models/cli.rb
@@ -8,29 +8,19 @@ class CLI
     def welcome 
         system('clear')
         puts "Welcome to After Party! You've been invited to a party at Julia's house!"
-        # might be nice to have a pause here
+        sleep(1.5) # nice to have a pause here
         self.login
     end
     
     def login 
         if @@prompt.yes?("Do you already have a login?") 
-            find_user
+            User.find_user
+            # returns this error: Could not find table 'users' (ActiveRecord::StatementInvalid)
         else
-            create_user_login
+            # returns this error: Could not find table 'users' (ActiveRecord::StatementInvalid)
+            User.create_user_login
         end
-        # @@prompt.ask("What is your name?")
     end
 
-    # def find_user
-    #     username = @@prompt.ask("What is your username?")
-    #     password = @@prompt.mask("What is your password?")
-    #     User.find_by(username: username, password: password)
-    # end
-    
-    # def create_user_login
-    #     username = @@prompt.ask("Set a username:")
-    #     password = @@prompt.mask("Select a password:")
-    #     User.create(username: username, password: password)
-    # end
 
 end #end of CLI class
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,15 @@ class User < ActiveRecord::Base
     has_many :login_sessions
     has_many :characters, through: :login_sessions
 
-    def find_user
+    @@prompt = TTY::Prompt.new
+
+    def self.find_user
         username = @@prompt.ask("What is your username?")
         password = @@prompt.mask("What is your password?")
         self.find_by(username: username, password: password)
     end
 
-    def create_user_login
+    def self.create_user_login
         username = @@prompt.ask("Set a username:")
         password = @@prompt.mask("Select a password:")
         User.create(username: username, password: password)
