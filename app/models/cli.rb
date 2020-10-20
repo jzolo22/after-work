8c72b41715eceb4d6d7a0ad80be9fe7893309f9c
jzolo22 <jzolotarev@gmail.com>
2020-10-19 20:52:30 -0400

    user-login

--- app/models/cli.rb
+++ app/models/cli.rb
@@ -8,10 +8,21 @@ class CLI
     def welcome 
         system('clear')
         puts "You've been invited to an after-work party at Julia's house!"
+        self.login
     end
     
+    def login 
+        # if @@prompt.yes?("Do you already have a login?") == "no" 
+        #     create_username
+        # end
+        # @@prompt.ask("What is your name?")
+    end
+    
+    def create_user_login
+        username = @@prompt.ask("Set a username:")
+        password = @@prompt.mask("Select a password:")
+        User.create(username: username, password: password)
 
-    @@prompt.ask("What is your name?", default: ENV["USER"])
-
+    end
 
 end #end of CLI class
