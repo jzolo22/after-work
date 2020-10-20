506b0a358b2234a1a133ec31279acca568a6d333
jzolo22 <jzolotarev@gmail.com>
2020-10-19 21:00:52 -0400

    moved login/find_user methods to user.rb

--- app/models/cli.rb
+++ app/models/cli.rb
@@ -7,22 +7,30 @@ class CLI
     
     def welcome 
         system('clear')
-        puts "You've been invited to an after-work party at Julia's house!"
+        puts "Welcome to After Party! You've been invited to a party at Julia's house!"
+        # might be nice to have a pause here
         self.login
     end
     
     def login 
-        # if @@prompt.yes?("Do you already have a login?") == "no" 
-        #     create_username
-        # end
+        if @@prompt.yes?("Do you already have a login?") 
+            find_user
+        else
+            create_user_login
+        end
         # @@prompt.ask("What is your name?")
     end
-    
-    def create_user_login
-        username = @@prompt.ask("Set a username:")
-        password = @@prompt.mask("Select a password:")
-        User.create(username: username, password: password)
 
-    end
+    # def find_user
+    #     username = @@prompt.ask("What is your username?")
+    #     password = @@prompt.mask("What is your password?")
+    #     User.find_by(username: username, password: password)
+    # end
+    
+    # def create_user_login
+    #     username = @@prompt.ask("Set a username:")
+    #     password = @@prompt.mask("Select a password:")
+    #     User.create(username: username, password: password)
+    # end
 
 end #end of CLI class
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ActiveRecord::Base
     has_many :login_sessions
     has_many :characters, through: :login_sessions
+
+    def find_user
+        username = @@prompt.ask("What is your username?")
+        password = @@prompt.mask("What is your password?")
+        self.find_by(username: username, password: password)
+    end
+
+    def create_user_login
+        username = @@prompt.ask("Set a username:")
+        password = @@prompt.mask("Select a password:")
+        User.create(username: username, password: password)
+    end
+
+
 end
